05f0ac5720412d062dc120b045cb7869f1b046b1
tabaker <tabaker@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2013-09-13 17:35:45 +0000

    Added CometSearchManagerWrapper class, constructor and destructor which reference a CometSearchManager object to be wrapper. Got the CometWrapper DLL building with these changes.
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@161 299d2d02-8f3b-485e-a07e-71e8e5e16577

--- branches/CometUI/comet-ms/CometWrapper/CometWrapper.cpp
+++ branches/CometUI/comet-ms/CometWrapper/CometWrapper.cpp
@@ -4,3 +4,26 @@
 
 #include "CometWrapper.h"
 
+#pragma region Includes
+#include "CometWrapper.h"
+using namespace CometWrapper;
+
+#include <msclr/marshal.h>
+using namespace msclr::interop;
+#pragma endregion
+
+
+CometSearchManagerWrapper::CometSearchManagerWrapper()
+{
+    // Instantiate the native C++ class CSimpleObject.
+    _pSearchMgr = new CometSearchManager();
+}
+
+CometSearchManagerWrapper::~CometSearchManagerWrapper()
+{
+    if (_pSearchMgr)
+    {
+        delete _pSearchMgr;
+        _pSearchMgr = NULL;
+    }
+}
--- branches/CometUI/comet-ms/CometWrapper/CometWrapper.h
+++ branches/CometUI/comet-ms/CometWrapper/CometWrapper.h
@@ -2,12 +2,21 @@
 
 #pragma once
 
+#include "Common.h"
+#include "CometSearchManager.h"
+
 using namespace System;
 
 namespace CometWrapper {
 
-	public ref class Class1
+	public ref class CometSearchManagerWrapper
 	{
+    public:
+        CometSearchManagerWrapper();
+        virtual ~CometSearchManagerWrapper();
 		// TODO: Add your methods for this class here.
+
+    private:
+        CometSearchManager *_pSearchMgr;
 	};
 }
