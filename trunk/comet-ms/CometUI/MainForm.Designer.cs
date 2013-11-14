2dcb7a63995103cfc3087d0be35154d22eda8f31
tabaker <tabaker@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2013-11-14 07:32:44 +0000

    Ongoing work on Comet UI - added main tab and input files tab page.
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@209 299d2d02-8f3b-485e-a07e-71e8e5e16577

--- trunk/comet-ms/CometUI/MainForm.Designer.cs
+++ trunk/comet-ms/CometUI/MainForm.Designer.cs
@@ -28,44 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.BtnTest = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItemHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.searchSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
-            this.inputFilesListBox = new System.Windows.Forms.ListBox();
-            this.inputFilesLabel = new System.Windows.Forms.Label();
+            this.btnBrowseProteomeDbFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.proteomeDbFileCombo = new System.Windows.Forms.ComboBox();
             this.btnRemFile = new System.Windows.Forms.Button();
             this.btnAddFile = new System.Windows.Forms.Button();
-            this.proteomeDbFileCombo = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnBrowseProteomeDbFile = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.inputFilesLabel = new System.Windows.Forms.Label();
+            this.inputFilesListBox = new System.Windows.Forms.ListBox();
+            this.cometMainTab = new System.Windows.Forms.TabControl();
+            this.inputFilesTabPage = new System.Windows.Forms.TabPage();
+            this.enzymeTabPage = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.mainMenuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).BeginInit();
             this.mainSplitContainer.Panel1.SuspendLayout();
             this.mainSplitContainer.Panel2.SuspendLayout();
             this.mainSplitContainer.SuspendLayout();
+            this.cometMainTab.SuspendLayout();
+            this.inputFilesTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
-            // BtnTest
+            // btnSearch
             // 
-            this.BtnTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnTest.Location = new System.Drawing.Point(462, 12);
-            this.BtnTest.Name = "BtnTest";
-            this.BtnTest.Size = new System.Drawing.Size(75, 23);
-            this.BtnTest.TabIndex = 0;
-            this.BtnTest.Text = "Test";
-            this.BtnTest.UseVisualStyleBackColor = true;
-            this.BtnTest.Click += new System.EventHandler(this.BtnTestClick);
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Location = new System.Drawing.Point(472, 0);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnTestClick);
             // 
             // mainMenuStrip
             // 
             this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.settingsToolStripMenuItem,
             this.toolStripMenuItemHelp});
             this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.mainMenuStrip.Name = "mainMenuStrip";
@@ -87,20 +88,6 @@
             this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.aboutToolStripMenuItem.Text = "&About";
             // 
-            // settingsToolStripMenuItem
-            // 
-            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.searchSettingsToolStripMenuItem});
-            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.settingsToolStripMenuItem.Text = "&Settings";
-            // 
-            // searchSettingsToolStripMenuItem
-            // 
-            this.searchSettingsToolStripMenuItem.Name = "searchSettingsToolStripMenuItem";
-            this.searchSettingsToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
-            this.searchSettingsToolStripMenuItem.Text = "&Search Parameters";
-            // 
             // mainSplitContainer
             // 
             this.mainSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -112,62 +99,35 @@
             // 
             // mainSplitContainer.Panel1
             // 
-            this.mainSplitContainer.Panel1.Controls.Add(this.label2);
-            this.mainSplitContainer.Panel1.Controls.Add(this.button1);
-            this.mainSplitContainer.Panel1.Controls.Add(this.btnBrowseProteomeDbFile);
-            this.mainSplitContainer.Panel1.Controls.Add(this.label1);
-            this.mainSplitContainer.Panel1.Controls.Add(this.proteomeDbFileCombo);
-            this.mainSplitContainer.Panel1.Controls.Add(this.btnRemFile);
-            this.mainSplitContainer.Panel1.Controls.Add(this.btnAddFile);
-            this.mainSplitContainer.Panel1.Controls.Add(this.inputFilesLabel);
-            this.mainSplitContainer.Panel1.Controls.Add(this.inputFilesListBox);
+            this.mainSplitContainer.Panel1.Controls.Add(this.cometMainTab);
             // 
             // mainSplitContainer.Panel2
             // 
-            this.mainSplitContainer.Panel2.Controls.Add(this.BtnTest);
+            this.mainSplitContainer.Panel2.Controls.Add(this.statusStrip1);
+            this.mainSplitContainer.Panel2.Controls.Add(this.btnSearch);
             this.mainSplitContainer.Size = new System.Drawing.Size(559, 413);
-            this.mainSplitContainer.SplitterDistance = 362;
+            this.mainSplitContainer.SplitterDistance = 358;
             this.mainSplitContainer.TabIndex = 2;
             // 
-            // inputFilesListBox
+            // btnBrowseProteomeDbFile
             // 
-            this.inputFilesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.inputFilesListBox.FormattingEnabled = true;
-            this.inputFilesListBox.Location = new System.Drawing.Point(12, 31);
-            this.inputFilesListBox.Name = "inputFilesListBox";
-            this.inputFilesListBox.Size = new System.Drawing.Size(444, 134);
-            this.inputFilesListBox.TabIndex = 0;
+            this.btnBrowseProteomeDbFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseProteomeDbFile.Enabled = false;
+            this.btnBrowseProteomeDbFile.Location = new System.Drawing.Point(440, 198);
+            this.btnBrowseProteomeDbFile.Name = "btnBrowseProteomeDbFile";
+            this.btnBrowseProteomeDbFile.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseProteomeDbFile.TabIndex = 9;
+            this.btnBrowseProteomeDbFile.Text = "&Browse";
+            this.btnBrowseProteomeDbFile.UseVisualStyleBackColor = true;
             // 
-            // inputFilesLabel
+            // label1
             // 
-            this.inputFilesLabel.AutoSize = true;
-            this.inputFilesLabel.Location = new System.Drawing.Point(12, 12);
-            this.inputFilesLabel.Name = "inputFilesLabel";
-            this.inputFilesLabel.Size = new System.Drawing.Size(58, 13);
-            this.inputFilesLabel.TabIndex = 1;
-            this.inputFilesLabel.Text = "&Input Files:";
-            // 
-            // btnRemFile
-            // 
-            this.btnRemFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRemFile.Enabled = false;
-            this.btnRemFile.Location = new System.Drawing.Point(462, 60);
-            this.btnRemFile.Name = "btnRemFile";
-            this.btnRemFile.Size = new System.Drawing.Size(75, 23);
-            this.btnRemFile.TabIndex = 5;
-            this.btnRemFile.Text = "&Remove";
-            this.btnRemFile.UseVisualStyleBackColor = true;
-            // 
-            // btnAddFile
-            // 
-            this.btnAddFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAddFile.Location = new System.Drawing.Point(462, 31);
-            this.btnAddFile.Name = "btnAddFile";
-            this.btnAddFile.Size = new System.Drawing.Size(75, 23);
-            this.btnAddFile.TabIndex = 4;
-            this.btnAddFile.Text = "&Add...";
-            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "&Proteome Database (.fasta):";
             // 
             // proteomeDbFileCombo
             // 
@@ -178,50 +138,98 @@
             this.proteomeDbFileCombo.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources;
             this.proteomeDbFileCombo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.proteomeDbFileCombo.FormattingEnabled = true;
-            this.proteomeDbFileCombo.Location = new System.Drawing.Point(12, 194);
+            this.proteomeDbFileCombo.Location = new System.Drawing.Point(7, 199);
             this.proteomeDbFileCombo.Name = "proteomeDbFileCombo";
-            this.proteomeDbFileCombo.Size = new System.Drawing.Size(444, 23);
+            this.proteomeDbFileCombo.Size = new System.Drawing.Size(427, 23);
             this.proteomeDbFileCombo.TabIndex = 6;
             // 
-            // label1
+            // btnRemFile
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 178);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(139, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "&Proteome Database (.fasta):";
+            this.btnRemFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemFile.Enabled = false;
+            this.btnRemFile.Location = new System.Drawing.Point(440, 56);
+            this.btnRemFile.Name = "btnRemFile";
+            this.btnRemFile.Size = new System.Drawing.Size(75, 23);
+            this.btnRemFile.TabIndex = 5;
+            this.btnRemFile.Text = "&Remove";
+            this.btnRemFile.UseVisualStyleBackColor = true;
             // 
-            // btnBrowseProteomeDbFile
+            // btnAddFile
             // 
-            this.btnBrowseProteomeDbFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBrowseProteomeDbFile.Enabled = false;
-            this.btnBrowseProteomeDbFile.Location = new System.Drawing.Point(462, 193);
-            this.btnBrowseProteomeDbFile.Name = "btnBrowseProteomeDbFile";
-            this.btnBrowseProteomeDbFile.Size = new System.Drawing.Size(75, 23);
-            this.btnBrowseProteomeDbFile.TabIndex = 9;
-            this.btnBrowseProteomeDbFile.Text = "&Browse";
-            this.btnBrowseProteomeDbFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddFile.Location = new System.Drawing.Point(440, 27);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFile.TabIndex = 4;
+            this.btnAddFile.Text = "&Add...";
+            this.btnAddFile.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // inputFilesLabel
             // 
-            this.button1.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(15, 245);
-            this.button1.Margin = new System.Windows.Forms.Padding(0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(20, 20);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "+";
-            this.button1.UseVisualStyleBackColor = true;
+            this.inputFilesLabel.AutoSize = true;
+            this.inputFilesLabel.Location = new System.Drawing.Point(7, 8);
+            this.inputFilesLabel.Name = "inputFilesLabel";
+            this.inputFilesLabel.Size = new System.Drawing.Size(58, 13);
+            this.inputFilesLabel.TabIndex = 1;
+            this.inputFilesLabel.Text = "&Input Files:";
             // 
-            // label2
+            // inputFilesListBox
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 248);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 13);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "Search Status";
+            this.inputFilesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputFilesListBox.FormattingEnabled = true;
+            this.inputFilesListBox.Location = new System.Drawing.Point(7, 27);
+            this.inputFilesListBox.Name = "inputFilesListBox";
+            this.inputFilesListBox.Size = new System.Drawing.Size(427, 121);
+            this.inputFilesListBox.TabIndex = 0;
+            // 
+            // cometMainTab
+            // 
+            this.cometMainTab.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cometMainTab.Controls.Add(this.inputFilesTabPage);
+            this.cometMainTab.Controls.Add(this.enzymeTabPage);
+            this.cometMainTab.Location = new System.Drawing.Point(12, 0);
+            this.cometMainTab.Name = "cometMainTab";
+            this.cometMainTab.SelectedIndex = 0;
+            this.cometMainTab.Size = new System.Drawing.Size(535, 356);
+            this.cometMainTab.TabIndex = 10;
+            // 
+            // inputFilesTabPage
+            // 
+            this.inputFilesTabPage.Controls.Add(this.label1);
+            this.inputFilesTabPage.Controls.Add(this.btnRemFile);
+            this.inputFilesTabPage.Controls.Add(this.btnBrowseProteomeDbFile);
+            this.inputFilesTabPage.Controls.Add(this.btnAddFile);
+            this.inputFilesTabPage.Controls.Add(this.proteomeDbFileCombo);
+            this.inputFilesTabPage.Controls.Add(this.inputFilesLabel);
+            this.inputFilesTabPage.Controls.Add(this.inputFilesListBox);
+            this.inputFilesTabPage.Location = new System.Drawing.Point(4, 22);
+            this.inputFilesTabPage.Name = "inputFilesTabPage";
+            this.inputFilesTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.inputFilesTabPage.Size = new System.Drawing.Size(527, 330);
+            this.inputFilesTabPage.TabIndex = 0;
+            this.inputFilesTabPage.Text = "Input Files";
+            this.inputFilesTabPage.UseVisualStyleBackColor = true;
+            // 
+            // enzymeTabPage
+            // 
+            this.enzymeTabPage.Location = new System.Drawing.Point(4, 22);
+            this.enzymeTabPage.Name = "enzymeTabPage";
+            this.enzymeTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.enzymeTabPage.Size = new System.Drawing.Size(527, 330);
+            this.enzymeTabPage.TabIndex = 1;
+            this.enzymeTabPage.Text = "Enzyme";
+            this.enzymeTabPage.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 29);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(559, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
             // 
             // CometMainForm
             // 
@@ -237,10 +245,13 @@
             this.mainMenuStrip.ResumeLayout(false);
             this.mainMenuStrip.PerformLayout();
             this.mainSplitContainer.Panel1.ResumeLayout(false);
-            this.mainSplitContainer.Panel1.PerformLayout();
             this.mainSplitContainer.Panel2.ResumeLayout(false);
+            this.mainSplitContainer.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).EndInit();
             this.mainSplitContainer.ResumeLayout(false);
+            this.cometMainTab.ResumeLayout(false);
+            this.inputFilesTabPage.ResumeLayout(false);
+            this.inputFilesTabPage.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -248,12 +259,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button BtnTest;
+        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.MenuStrip mainMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemHelp;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem searchSettingsToolStripMenuItem;
         private System.Windows.Forms.SplitContainer mainSplitContainer;
         private System.Windows.Forms.ListBox inputFilesListBox;
         private System.Windows.Forms.Label inputFilesLabel;
@@ -262,8 +271,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox proteomeDbFileCombo;
         private System.Windows.Forms.Button btnBrowseProteomeDbFile;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl cometMainTab;
+        private System.Windows.Forms.TabPage inputFilesTabPage;
+        private System.Windows.Forms.TabPage enzymeTabPage;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
