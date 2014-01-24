25044e11c23b195d5e6b2a22f1a2ec01a9fb56eb
tabaker <tabaker@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2014-01-24 23:49:55 +0000

    Backing up changes for ongoing development of the Comet UI.
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@262 299d2d02-8f3b-485e-a07e-71e8e5e16577

---
+++ trunk/comet-ms/CometUI/MassSettingsControl.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace CometUI
+{
+    partial class MassSettingsControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.precursorGroupBox = new System.Windows.Forms.GroupBox();
+            this.fragmentGroupBox = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxDecoyPrefix = new System.Windows.Forms.TextBox();
+            this.labelMassTol = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.massUnitsPanel = new System.Windows.Forms.Panel();
+            this.massPanel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.precursorGroupBox.SuspendLayout();
+            this.massUnitsPanel.SuspendLayout();
+            this.massPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // precursorGroupBox
+            // 
+            this.precursorGroupBox.Controls.Add(this.panel2);
+            this.precursorGroupBox.Controls.Add(this.panel1);
+            this.precursorGroupBox.Controls.Add(this.massPanel2);
+            this.precursorGroupBox.Controls.Add(this.massUnitsPanel);
+            this.precursorGroupBox.Controls.Add(this.textBoxDecoyPrefix);
+            this.precursorGroupBox.Controls.Add(this.labelMassTol);
+            this.precursorGroupBox.Location = new System.Drawing.Point(26, 17);
+            this.precursorGroupBox.Name = "precursorGroupBox";
+            this.precursorGroupBox.Size = new System.Drawing.Size(225, 288);
+            this.precursorGroupBox.TabIndex = 0;
+            this.precursorGroupBox.TabStop = false;
+            this.precursorGroupBox.Text = "Precursor";
+            // 
+            // fragmentGroupBox
+            // 
+            this.fragmentGroupBox.Location = new System.Drawing.Point(275, 17);
+            this.fragmentGroupBox.Name = "fragmentGroupBox";
+            this.fragmentGroupBox.Size = new System.Drawing.Size(225, 135);
+            this.fragmentGroupBox.TabIndex = 1;
+            this.fragmentGroupBox.TabStop = false;
+            this.fragmentGroupBox.Text = "Fragment";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(275, 168);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 137);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ions";
+            // 
+            // textBoxDecoyPrefix
+            // 
+            this.textBoxDecoyPrefix.Location = new System.Drawing.Point(45, 26);
+            this.textBoxDecoyPrefix.Name = "textBoxDecoyPrefix";
+            this.textBoxDecoyPrefix.Size = new System.Drawing.Size(74, 20);
+            this.textBoxDecoyPrefix.TabIndex = 12;
+            // 
+            // labelMassTol
+            // 
+            this.labelMassTol.AutoSize = true;
+            this.labelMassTol.Location = new System.Drawing.Point(18, 29);
+            this.labelMassTol.Name = "labelMassTol";
+            this.labelMassTol.Size = new System.Drawing.Size(25, 13);
+            this.labelMassTol.TabIndex = 11;
+            this.labelMassTol.Text = "Tol:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(2, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "amu";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(67, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ppm";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(124, 3);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(47, 17);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "mmu";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(2, 3);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(48, 17);
+            this.radioButton4.TabIndex = 16;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "MH+";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(67, 3);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(46, 17);
+            this.radioButton5.TabIndex = 17;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "M/Z";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // massUnitsPanel
+            // 
+            this.massUnitsPanel.Controls.Add(this.radioButton1);
+            this.massUnitsPanel.Controls.Add(this.radioButton2);
+            this.massUnitsPanel.Controls.Add(this.radioButton3);
+            this.massUnitsPanel.Location = new System.Drawing.Point(45, 56);
+            this.massUnitsPanel.Name = "massUnitsPanel";
+            this.massUnitsPanel.Size = new System.Drawing.Size(174, 23);
+            this.massUnitsPanel.TabIndex = 18;
+            // 
+            // massPanel2
+            // 
+            this.massPanel2.Controls.Add(this.radioButton4);
+            this.massPanel2.Controls.Add(this.radioButton5);
+            this.massPanel2.Location = new System.Drawing.Point(45, 85);
+            this.massPanel2.Name = "massPanel2";
+            this.massPanel2.Size = new System.Drawing.Size(174, 23);
+            this.massPanel2.TabIndex = 19;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton6);
+            this.panel1.Controls.Add(this.radioButton7);
+            this.panel1.Location = new System.Drawing.Point(45, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(174, 23);
+            this.panel1.TabIndex = 20;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(2, 3);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(43, 17);
+            this.radioButton6.TabIndex = 16;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "avg";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(67, 3);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(51, 17);
+            this.radioButton7.TabIndex = 17;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "mono";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButton8);
+            this.panel2.Controls.Add(this.radioButton9);
+            this.panel2.Location = new System.Drawing.Point(45, 143);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(174, 23);
+            this.panel2.TabIndex = 21;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(2, 3);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(59, 17);
+            this.radioButton8.TabIndex = 16;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "no C13";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(67, 3);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(78, 17);
+            this.radioButton9.TabIndex = 17;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "C13 offsets";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // MassSettingsControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.fragmentGroupBox);
+            this.Controls.Add(this.precursorGroupBox);
+            this.Name = "MassSettingsControl";
+            this.Size = new System.Drawing.Size(527, 330);
+            this.precursorGroupBox.ResumeLayout(false);
+            this.precursorGroupBox.PerformLayout();
+            this.massUnitsPanel.ResumeLayout(false);
+            this.massUnitsPanel.PerformLayout();
+            this.massPanel2.ResumeLayout(false);
+            this.massPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox precursorGroupBox;
+        private System.Windows.Forms.GroupBox fragmentGroupBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxDecoyPrefix;
+        private System.Windows.Forms.Label labelMassTol;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Panel massUnitsPanel;
+        private System.Windows.Forms.Panel massPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+    }
+}
---
+++ trunk/comet-ms/CometUI/MassSettingsControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CometUI
+{
+    public partial class MassSettingsControl : UserControl
+    {
+        private new Form Parent { get; set; }
+
+        public MassSettingsControl(Form parent)
+        {
+            InitializeComponent();
+
+            InitializeFromDefaultSettings();
+        }
+
+        private void InitializeFromDefaultSettings()
+        {
+        }
+    }
+}
