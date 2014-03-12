46e4bf13505005813128f7046877a20250fe36c5
eng_jk <eng_jk@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2014-03-12 00:03:59 +0000

    set version string; update project settings and remove unused UI code
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@320 299d2d02-8f3b-485e-a07e-71e8e5e16577

--- branches/release_2014010/CometSearch/Common.h
+++ branches/release_2014010/CometSearch/Common.h
@@ -43,7 +43,7 @@ using namespace std;
 #include <iostream>
 #endif
 
-#define comet_version   "2014.01 rev. ??"
+#define comet_version   "2014.01 rev. 0"
 #define copyright "(c) University of Washington"
 
 // Redefined how the bin offset is interpreted and applied.  The valid range for the offset is
