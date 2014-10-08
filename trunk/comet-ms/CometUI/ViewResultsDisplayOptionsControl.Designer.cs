ad4521b948f769e3f020d09de55b994f55313b82
tabaker <tabaker@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2014-10-08 20:26:54 +0000

    CometUI Ongoing Development: Check in some missing files.
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@543 299d2d02-8f3b-485e-a07e-71e8e5e16577

---
+++ trunk/comet-ms/CometUI/ViewResultsDisplayOptionsControl.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace CometUI
+{
+    partial class ViewResultsDisplayOptionsControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ViewResultsDisplayOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ViewResultsDisplayOptions";
+            this.Size = new System.Drawing.Size(653, 127);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
