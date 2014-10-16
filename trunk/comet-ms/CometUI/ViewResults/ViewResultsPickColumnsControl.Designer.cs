99813334b5244c79b94e76c73d5ff9f0ed292ec9
tabaker <tabaker@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2014-10-16 17:53:53 +0000

    CometUI Ongoing Development: Checking in some work on View Results Pick Column options tab.
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@553 299d2d02-8f3b-485e-a07e-71e8e5e16577

--- trunk/comet-ms/CometUI/ViewResults/ViewResultsPickColumnsControl.Designer.cs
+++ trunk/comet-ms/CometUI/ViewResults/ViewResultsPickColumnsControl.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.pickColumnsMainPanel = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnMoveDown = new System.Windows.Forms.Button();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnMoveToShowColumns = new System.Windows.Forms.Button();
+            this.btnMoveToHideColumns = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.hiddenColumnsLabel = new System.Windows.Forms.Label();
             this.btnUpdateResults = new System.Windows.Forms.Button();
@@ -44,10 +44,10 @@
             // pickColumnsMainPanel
             // 
             this.pickColumnsMainPanel.AutoScroll = true;
-            this.pickColumnsMainPanel.Controls.Add(this.button4);
-            this.pickColumnsMainPanel.Controls.Add(this.button3);
-            this.pickColumnsMainPanel.Controls.Add(this.button2);
-            this.pickColumnsMainPanel.Controls.Add(this.button1);
+            this.pickColumnsMainPanel.Controls.Add(this.btnMoveDown);
+            this.pickColumnsMainPanel.Controls.Add(this.btnMoveUp);
+            this.pickColumnsMainPanel.Controls.Add(this.btnMoveToShowColumns);
+            this.pickColumnsMainPanel.Controls.Add(this.btnMoveToHideColumns);
             this.pickColumnsMainPanel.Controls.Add(this.label1);
             this.pickColumnsMainPanel.Controls.Add(this.hiddenColumnsLabel);
             this.pickColumnsMainPanel.Controls.Add(this.btnUpdateResults);
@@ -59,41 +59,43 @@
             this.pickColumnsMainPanel.Size = new System.Drawing.Size(653, 220);
             this.pickColumnsMainPanel.TabIndex = 0;
             // 
-            // button4
+            // btnMoveDown
             // 
-            this.button4.Location = new System.Drawing.Point(561, 84);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 51;
-            this.button4.Text = "Down";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnMoveDown.Location = new System.Drawing.Point(561, 84);
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveDown.TabIndex = 51;
+            this.btnMoveDown.Text = "Down";
+            this.btnMoveDown.UseVisualStyleBackColor = true;
+            this.btnMoveDown.Click += new System.EventHandler(this.BtnMoveDownClick);
             // 
-            // button3
+            // btnMoveUp
             // 
-            this.button3.Location = new System.Drawing.Point(561, 45);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 50;
-            this.button3.Text = "Up";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnMoveUp.Location = new System.Drawing.Point(561, 45);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveUp.TabIndex = 50;
+            this.btnMoveUp.Text = "Up";
+            this.btnMoveUp.UseVisualStyleBackColor = true;
+            this.btnMoveUp.Click += new System.EventHandler(this.BtnMoveUpClick);
             // 
-            // button2
+            // btnMoveToShowColumns
             // 
-            this.button2.Location = new System.Drawing.Point(247, 84);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 49;
-            this.button2.Text = ">>";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnMoveToShowColumns.Location = new System.Drawing.Point(247, 84);
+            this.btnMoveToShowColumns.Name = "btnMoveToShowColumns";
+            this.btnMoveToShowColumns.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveToShowColumns.TabIndex = 49;
+            this.btnMoveToShowColumns.Text = ">>";
+            this.btnMoveToShowColumns.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnMoveToHideColumns
             // 
-            this.button1.Location = new System.Drawing.Point(247, 45);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 48;
-            this.button1.Text = "<<";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnMoveToHideColumns.Location = new System.Drawing.Point(247, 45);
+            this.btnMoveToHideColumns.Name = "btnMoveToHideColumns";
+            this.btnMoveToHideColumns.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveToHideColumns.TabIndex = 48;
+            this.btnMoveToHideColumns.Text = "<<";
+            this.btnMoveToHideColumns.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -126,6 +128,16 @@
             // showColumnsListBox
             // 
             this.showColumnsListBox.FormattingEnabled = true;
+            this.showColumnsListBox.Items.AddRange(new object[] {
+            "probability",
+            "spectrum",
+            "start_scan",
+            "spscore",
+            "ions2",
+            "peptide",
+            "protein",
+            "calc_neutral_pep_mass",
+            "xpress"});
             this.showColumnsListBox.Location = new System.Drawing.Point(339, 45);
             this.showColumnsListBox.Name = "showColumnsListBox";
             this.showColumnsListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
@@ -136,6 +148,29 @@
             // hiddenColumnsListBox
             // 
             this.hiddenColumnsListBox.FormattingEnabled = true;
+            this.hiddenColumnsListBox.Items.AddRange(new object[] {
+            "index",
+            "assumed_charge",
+            "precursor_neutral_mass",
+            "MZratio",
+            "protein_descr",
+            "pl",
+            "retention_time_sec",
+            "compensation_voltage",
+            "precursor_intensity",
+            "collision_energy",
+            "ppm",
+            "xcorr",
+            "deltacn",
+            "deltacnstar",
+            "sprank",
+            "ions",
+            "num_tol_term",
+            "num_missed_cleavages",
+            "massdiff",
+            "light_area",
+            "heavy_area",
+            "fval"});
             this.hiddenColumnsListBox.Location = new System.Drawing.Point(26, 45);
             this.hiddenColumnsListBox.Name = "hiddenColumnsListBox";
             this.hiddenColumnsListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
@@ -164,9 +199,9 @@
         private System.Windows.Forms.Button btnUpdateResults;
         private System.Windows.Forms.Label hiddenColumnsLabel;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnMoveDown;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnMoveToShowColumns;
+        private System.Windows.Forms.Button btnMoveToHideColumns;
     }
 }
--- trunk/comet-ms/CometUI/ViewResults/ViewResultsPickColumnsControl.cs
+++ trunk/comet-ms/CometUI/ViewResults/ViewResultsPickColumnsControl.cs
@@ -1,65 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace CometUI.ViewResults
 {
     public partial class ViewResultsPickColumnsControl : UserControl
     {
-        private readonly List<Column> _columnsList = new List<Column>();
         public ViewResultsPickColumnsControl()
         {
             InitializeComponent();
+        }
 
-            InitializeColumnsList();
-
-            foreach (var item in _columnsList)
+        private void MoveUp(ListBox listBox)
+        {
+            int selectedIndex = listBox.SelectedIndex;
+            if (selectedIndex > 0 & selectedIndex != -1)
             {
-                if (item.Show)
-                {
-                    showColumnsListBox.Items.Add(item.Name);
-                }
-                else
-                {
-                    hiddenColumnsListBox.Items.Add(item.Name);
-                }
+                listBox.Items.Insert(selectedIndex - 1, listBox.Items[selectedIndex]);
+                listBox.Items.RemoveAt(selectedIndex + 1);
+                listBox.SelectedIndex = selectedIndex - 1;
             }
         }
 
-        private void InitializeColumnsList()
+        private void MoveDown(ListBox listBox)
         {
-            _columnsList.Add(new Column("index", false));
-            _columnsList.Add(new Column("assumed_charge", false));
-            _columnsList.Add(new Column("precursor_neutral_mass", false));
-            _columnsList.Add(new Column("MZratio", false));
-            _columnsList.Add(new Column("protein_descr", false));
-            _columnsList.Add(new Column("pl", false));
-            _columnsList.Add(new Column("retention_time_sec", false));
-            _columnsList.Add(new Column("compensation_voltage", false));
-            _columnsList.Add(new Column("precursor_intensity", false));
-            _columnsList.Add(new Column("collision_energy", false));
-            _columnsList.Add(new Column("ppm", false));
-            _columnsList.Add(new Column("xcorr", false));
-            _columnsList.Add(new Column("deltacn", false));
-            _columnsList.Add(new Column("deltacnstar", false));
-            _columnsList.Add(new Column("sprank", false));
-            _columnsList.Add(new Column("ions", false));
-            _columnsList.Add(new Column("num_tol_term", false));
-            _columnsList.Add(new Column("num_missed_cleavages", false));
-            _columnsList.Add(new Column("massdiff", false));
-            _columnsList.Add(new Column("light_area", false));
-            _columnsList.Add(new Column("heavy_area", false));
-            _columnsList.Add(new Column("fval", false));
+            int selectedIndex = listBox.SelectedIndex;
+            if (selectedIndex < listBox.Items.Count - 1 & selectedIndex != -1)
+            {
+                listBox.Items.Insert(selectedIndex + 2, listBox.Items[selectedIndex]);
+                listBox.Items.RemoveAt(selectedIndex);
+                listBox.SelectedIndex = selectedIndex + 1;
 
-            _columnsList.Add(new Column("probability", true));
-            _columnsList.Add(new Column("spectrum", true));
-            _columnsList.Add(new Column("start_scan", true));
-            _columnsList.Add(new Column("spscore", true));
-            _columnsList.Add(new Column("ions2", true));
-            _columnsList.Add(new Column("peptide", true));
-            _columnsList.Add(new Column("protein", true));
-            _columnsList.Add(new Column("calc_neutral_pep_mass", true));
-            _columnsList.Add(new Column("xpress", true));
+            }
         }
 
         private void HiddenColumnsListBoxKeyUp(object sender, KeyEventArgs e)
@@ -83,16 +53,14 @@ namespace CometUI.ViewResults
             }
         }
 
-        class Column
+        private void BtnMoveUpClick(object sender, System.EventArgs e)
         {
-            public String Name { get; set; }
-            public bool Show { get; set; }
+            MoveUp(showColumnsListBox);
+        }
 
-            public Column(String name, bool show)
-            {
-                Name = name;
-                Show = show;
-            }
+        private void BtnMoveDownClick(object sender, System.EventArgs e)
+        {
+            MoveDown(showColumnsListBox);
         }
     }
 }
