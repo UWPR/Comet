0d73cfd7ed6977cb22d4368deabe41f2edab6e2e
tabaker <tabaker@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2015-01-06 20:16:21 +0000

    CometUI Ongoing Development: Backing up some changes View Results.
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@603 299d2d02-8f3b-485e-a07e-71e8e5e16577

--- trunk/comet-ms/CometUI/ViewResults/ViewSearchResultsControl.cs
+++ trunk/comet-ms/CometUI/ViewResults/ViewSearchResultsControl.cs
@@ -111,7 +111,30 @@ namespace CometUI.ViewResults
             resultsListView.Clear();
 
             UpdateColumnHeaders();
-            
+
+            //_condensedColumnHeadersMap.Add("index", "#");
+            //_condensedColumnHeadersMap.Add("assumed_charge", "Z");
+            //_condensedColumnHeadersMap.Add("precursor_neutral_mass", "EXP_MASS");
+            //_condensedColumnHeadersMap.Add("probability", "PROB");
+            //_condensedColumnHeadersMap.Add("start_scan", "SSCAN");
+            //_condensedColumnHeadersMap.Add("calc_neutral_pep_mass", "CALC_MASS");
+
+            //_columnHeadersMap.Add("MZratio", "MZRATIO");
+            //_columnHeadersMap.Add("protein_descr", "PROTEIN_DESCR");
+            //_columnHeadersMap.Add("pI", "PI");
+            //_columnHeadersMap.Add("retention_time_sec", "RETENTION_TIME_SEC");
+            //_columnHeadersMap.Add("precursor_intensity", "PRECURSOR_INTENSITY");
+            //_columnHeadersMap.Add("ppm", "PPM");
+            //_columnHeadersMap.Add("xcorr", "XCORR");
+            //_columnHeadersMap.Add("deltacn", "DELTACN");
+            //_columnHeadersMap.Add("deltacnstar", "DELTACNSTAR");
+            //_columnHeadersMap.Add("spectrum", "SPECTRUM");
+            //_columnHeadersMap.Add("spscore", "SPSCORE");
+            //_columnHeadersMap.Add("ions2", "IONS2");
+            //_columnHeadersMap.Add("peptide", "PEPTIDE");
+            //_columnHeadersMap.Add("protein", "PROTEIN");
+            //_columnHeadersMap.Add("xpress", "XPRESS");
+
             foreach (var searchResult in SearchResults)
             {
                 var row = new List<string>();
@@ -124,6 +147,12 @@ namespace CometUI.ViewResults
                         case "INDEX":
                             var index = (TypedSearchResultField<int>)searchResult.Fields["index"];
                             row.Add(Convert.ToString(index.Value));
+                            break;
+
+                        case "ASSUMED_CHARGE":
+                        case "Z":
+                            var charge = (TypedSearchResultField<int>)searchResult.Fields["assumed_charge"];
+                            row.Add(Convert.ToString(charge.Value));
                             break;
 
                         case "SPECTRUM":
@@ -574,29 +603,6 @@ namespace CometUI.ViewResults
             }
         }
     }
-
-    //_condensedColumnHeadersMap.Add("index", "#");
-    //_condensedColumnHeadersMap.Add("assumed_charge", "Z");
-    //_condensedColumnHeadersMap.Add("precursor_neutral_mass", "EXP_MASS");
-    //_condensedColumnHeadersMap.Add("probability", "PROB");
-    //_condensedColumnHeadersMap.Add("start_scan", "SSCAN");
-    //_condensedColumnHeadersMap.Add("calc_neutral_pep_mass", "CALC_MASS");
-
-    //_columnHeadersMap.Add("MZratio", "MZRATIO");
-    //_columnHeadersMap.Add("protein_descr", "PROTEIN_DESCR");
-    //_columnHeadersMap.Add("pI", "PI");
-    //_columnHeadersMap.Add("retention_time_sec", "RETENTION_TIME_SEC");
-    //_columnHeadersMap.Add("precursor_intensity", "PRECURSOR_INTENSITY");
-    //_columnHeadersMap.Add("ppm", "PPM");
-    //_columnHeadersMap.Add("xcorr", "XCORR");
-    //_columnHeadersMap.Add("deltacn", "DELTACN");
-    //_columnHeadersMap.Add("deltacnstar", "DELTACNSTAR");
-    //_columnHeadersMap.Add("spectrum", "SPECTRUM");
-    //_columnHeadersMap.Add("spscore", "SPSCORE");
-    //_columnHeadersMap.Add("ions2", "IONS2");
-    //_columnHeadersMap.Add("peptide", "PEPTIDE");
-    //_columnHeadersMap.Add("protein", "PROTEIN");
-    //_columnHeadersMap.Add("xpress", "XPRESS");
 
     public interface ISearchResultField
     {
