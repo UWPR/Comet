d8274edef6df3d7edef1040ab81f1767e13c76d3
tabaker <tabaker@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2015-07-27 17:27:30 +0000

    Made following changes:
    Added release version. 
    Change the CometUI publish location. 
    Changed CometUI Application revision.
    
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@788 299d2d02-8f3b-485e-a07e-71e8e5e16577

--- branches/release_2015020_ui/comet-ms/CometSearch/Common.h
+++ branches/release_2015020_ui/comet-ms/CometSearch/Common.h
@@ -45,7 +45,7 @@ using namespace std;
 #include <iostream>
 #endif
 
-#define comet_version   "201X.XX rev. X"
+#define comet_version   "2015.02 rev. 0"
 #define copyright "(c) University of Washington"
 
 // Redefined how the bin offset is interpreted and applied.  The valid range for the offset is
