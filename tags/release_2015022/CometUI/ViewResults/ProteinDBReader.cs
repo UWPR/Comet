d4e924b6d1e3fce5d6e1626d51c43fa58021fc4d
eng_jk <eng_jk@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2015-10-15 20:46:34 +0000

    tagging release 2015.02 rev. 2
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@861 299d2d02-8f3b-485e-a07e-71e8e5e16577

---
+++ tags/release_2015022/CometUI/ViewResults/ProteinDBReader.cs
@@ -0,0 +1,66 @@
+﻿/*
+   Copyright 2015 University of Washington
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.IO;
+
+namespace CometUI.ViewResults
+{
+    class ProteinDBReader
+    {
+        private String FileName { get; set; }
+
+        public ProteinDBReader(String fileName)
+        {
+            FileName = fileName;
+        }
+
+        /// <summary>
+        /// Reads the protein sequence and its header from the protein database
+        /// file specified in FileName.
+        /// NOTE: This method throws exceptions the caller must handle.
+        /// </summary>
+        /// <param name="proteinName"> The name of the protein to look up. </param>
+        /// <returns> Sucess: The protein sequence and its header,
+        /// Failed: null </returns>
+        public String ReadProtein(String proteinName)
+        {
+            String returnStr = null;
+            // Create a StreamReader to read from the protein database file.
+            // The using statement also closes the StreamReader.
+            using (var sr = new StreamReader(FileName))
+            {
+                String line;
+                // Read and display lines from the file until the end of 
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains(proteinName))
+                    {
+                        returnStr = line + Environment.NewLine;
+                        while (((line = sr.ReadLine()) != null) && (!line.Contains(">")))
+                        {
+                            returnStr += line;
+                        }
+                        break;
+                    }
+                }
+            }
+           
+            return returnStr;
+        }
+    }
+}
