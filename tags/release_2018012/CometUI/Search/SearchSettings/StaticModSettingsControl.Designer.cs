0103756ec9d244337ab6943abce6ad21a3e78ad5
eng_jk <eng_jk@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2018-06-13 15:29:33 +0000

    tagging release 2018.01 rev. 2
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@1173 299d2d02-8f3b-485e-a07e-71e8e5e16577

---
+++ tags/release_2018012/CometUI/Search/SearchSettings/StaticModSettingsControl.Designer.cs
@@ -0,0 +1,216 @@
+﻿using CometUI.SharedUI;
+
+namespace CometUI.Search.SearchSettings
+{
+    partial class StaticModSettingsControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.staticModsDataGridView = new System.Windows.Forms.DataGridView();
+            this.staticModsNameCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.staticModsResidueCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.staticModsMassDiffCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.staticCTermPeptideTextBox = new NumericTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.staticNTermPeptideTextBox = new NumericTextBox();
+            this.staticCTermProteinTextBox = new NumericTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.staticNTermProteinTextBox = new NumericTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.staticModsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // staticModsDataGridView
+            // 
+            this.staticModsDataGridView.AllowUserToAddRows = false;
+            this.staticModsDataGridView.AllowUserToDeleteRows = false;
+            this.staticModsDataGridView.AllowUserToResizeRows = false;
+            this.staticModsDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.staticModsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.staticModsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.staticModsNameCol,
+            this.staticModsResidueCol,
+            this.staticModsMassDiffCol});
+            this.staticModsDataGridView.Location = new System.Drawing.Point(26, 26);
+            this.staticModsDataGridView.Name = "staticModsDataGridView";
+            this.staticModsDataGridView.Size = new System.Drawing.Size(432, 287);
+            this.staticModsDataGridView.TabIndex = 1;
+            this.staticModsDataGridView.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.StaticModsDataGridViewCellEndEdit);
+            // 
+            // staticModsNameCol
+            // 
+            this.staticModsNameCol.HeaderText = "Name";
+            this.staticModsNameCol.Name = "staticModsNameCol";
+            this.staticModsNameCol.ReadOnly = true;
+            this.staticModsNameCol.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.staticModsNameCol.Width = 124;
+            // 
+            // staticModsResidueCol
+            // 
+            this.staticModsResidueCol.HeaderText = "Residue";
+            this.staticModsResidueCol.Name = "staticModsResidueCol";
+            this.staticModsResidueCol.ReadOnly = true;
+            this.staticModsResidueCol.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.staticModsResidueCol.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.staticModsResidueCol.Width = 124;
+            // 
+            // staticModsMassDiffCol
+            // 
+            this.staticModsMassDiffCol.HeaderText = "Mass Diff";
+            this.staticModsMassDiffCol.Name = "staticModsMassDiffCol";
+            this.staticModsMassDiffCol.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.staticModsMassDiffCol.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.staticModsMassDiffCol.Width = 124;
+            // 
+            // staticCTermPeptideTextBox
+            // 
+            this.staticCTermPeptideTextBox.AllowDecimal = true;
+            this.staticCTermPeptideTextBox.AllowGroupSeparator = false;
+            this.staticCTermPeptideTextBox.AllowNegative = false;
+            this.staticCTermPeptideTextBox.AllowSpace = false;
+            this.staticCTermPeptideTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.staticCTermPeptideTextBox.Location = new System.Drawing.Point(128, 372);
+            this.staticCTermPeptideTextBox.Name = "staticCTermPeptideTextBox";
+            this.staticCTermPeptideTextBox.Size = new System.Drawing.Size(86, 20);
+            this.staticCTermPeptideTextBox.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "N-terminus Peptide:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 375);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "C-terminus Peptide:";
+            // 
+            // staticNTermPeptideTextBox
+            // 
+            this.staticNTermPeptideTextBox.AllowDecimal = true;
+            this.staticNTermPeptideTextBox.AllowGroupSeparator = false;
+            this.staticNTermPeptideTextBox.AllowNegative = false;
+            this.staticNTermPeptideTextBox.AllowSpace = false;
+            this.staticNTermPeptideTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.staticNTermPeptideTextBox.Location = new System.Drawing.Point(128, 346);
+            this.staticNTermPeptideTextBox.Name = "staticNTermPeptideTextBox";
+            this.staticNTermPeptideTextBox.Size = new System.Drawing.Size(86, 20);
+            this.staticNTermPeptideTextBox.TabIndex = 11;
+            // 
+            // staticCTermProteinTextBox
+            // 
+            this.staticCTermProteinTextBox.AllowDecimal = true;
+            this.staticCTermProteinTextBox.AllowGroupSeparator = false;
+            this.staticCTermProteinTextBox.AllowNegative = false;
+            this.staticCTermProteinTextBox.AllowSpace = false;
+            this.staticCTermProteinTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.staticCTermProteinTextBox.Location = new System.Drawing.Point(372, 372);
+            this.staticCTermProteinTextBox.Name = "staticCTermProteinTextBox";
+            this.staticCTermProteinTextBox.Size = new System.Drawing.Size(86, 20);
+            this.staticCTermProteinTextBox.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "N-terminus Protein:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(270, 375);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "C-terminus Protein:";
+            // 
+            // staticNTermProteinTextBox
+            // 
+            this.staticNTermProteinTextBox.AllowDecimal = true;
+            this.staticNTermProteinTextBox.AllowGroupSeparator = false;
+            this.staticNTermProteinTextBox.AllowNegative = false;
+            this.staticNTermProteinTextBox.AllowSpace = false;
+            this.staticNTermProteinTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.staticNTermProteinTextBox.Location = new System.Drawing.Point(372, 345);
+            this.staticNTermProteinTextBox.Name = "staticNTermProteinTextBox";
+            this.staticNTermProteinTextBox.Size = new System.Drawing.Size(86, 20);
+            this.staticNTermProteinTextBox.TabIndex = 15;
+            // 
+            // StaticModSettingsControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.staticCTermProteinTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.staticNTermProteinTextBox);
+            this.Controls.Add(this.staticCTermPeptideTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.staticNTermPeptideTextBox);
+            this.Controls.Add(this.staticModsDataGridView);
+            this.Name = "StaticModSettingsControl";
+            this.Size = new System.Drawing.Size(527, 450);
+            ((System.ComponentModel.ISupportInitialize)(this.staticModsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView staticModsDataGridView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn staticModsNameCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn staticModsResidueCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn staticModsMassDiffCol;
+        private NumericTextBox staticCTermPeptideTextBox;
+        private NumericTextBox staticNTermPeptideTextBox;
+        private NumericTextBox staticCTermProteinTextBox;
+        private NumericTextBox staticNTermProteinTextBox;
+
+    }
+}
---
+++ tags/release_2018012/CometUI/ViewResults/ExportSearchResults.cs
@@ -0,0 +1,63 @@
+﻿/*
+   Copyright 2015 University of Washington
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.IO;
+using BrightIdeasSoftware;
+
+namespace CometUI.ViewResults
+{
+    class ExportSearchResults
+    {
+        public void Export(ObjectListView resultsList, String exportFile)
+        {
+            using (var file = new StreamWriter(exportFile))
+            {
+                  WriteHeader(file, resultsList);
+                  WriteResults(file, resultsList);
+            }
+        }
+
+        private void WriteHeader(StreamWriter file, ObjectListView resultsList)
+        {
+            var header = String.Empty;
+            foreach (OLVColumn column in resultsList.Columns)
+            {
+                header += column.Text + '\t';
+            }
+
+            file.WriteLine(header);
+            file.Flush();
+        }
+
+        private void WriteResults(StreamWriter file, ObjectListView resultsList)
+        {
+           //foreach (OLVListItem item in resultsList.Items)
+           for (int i=0; i<resultsList.Items.Count; i++)
+           {
+                var result = String.Empty;
+                //foreach (OLVListSubItem subItem in item.SubItems)
+                foreach (OLVListSubItem subItem in resultsList.Items[i].SubItems)
+                {
+                    result += subItem.Text + '\t';
+                }
+
+                file.WriteLine(result);
+                file.Flush();
+            }
+        }
+    }
+}
