65e13cae08cf06e5897884263c3592a21001a66a
eng_jk <eng_jk@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2019-08-20 20:51:05 +0000

    tagging release 2019.01 rev. 0
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@1329 299d2d02-8f3b-485e-a07e-71e8e5e16577

---
+++ tags/release_2019010/CometUI/SharedUI/ExportFileDlg.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CometUI.SharedUI
+{
+    partial class ExportFileDlg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExportFileDlg));
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.filePathLabel = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnBrowseFilePath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(15, 36);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(397, 20);
+            this.fileNameTextBox.TabIndex = 0;
+            this.fileNameTextBox.TextChanged += new System.EventHandler(this.FileNameTextBoxTextChanged);
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(15, 90);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(397, 20);
+            this.filePathTextBox.TabIndex = 1;
+            this.filePathTextBox.TextChanged += new System.EventHandler(this.FilePathTextBoxTextChanged);
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(12, 20);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(57, 13);
+            this.fileNameLabel.TabIndex = 2;
+            this.fileNameLabel.Text = "File Name:";
+            // 
+            // filePathLabel
+            // 
+            this.filePathLabel.AutoSize = true;
+            this.filePathLabel.Location = new System.Drawing.Point(12, 74);
+            this.filePathLabel.Name = "filePathLabel";
+            this.filePathLabel.Size = new System.Drawing.Size(51, 13);
+            this.filePathLabel.TabIndex = 3;
+            this.filePathLabel.Text = "File Path:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(286, 137);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSaveClick);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(367, 137);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancelClick);
+            // 
+            // btnBrowseFilePath
+            // 
+            this.btnBrowseFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseFilePath.BackColor = System.Drawing.Color.Transparent;
+            this.btnBrowseFilePath.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBrowseFilePath.BackgroundImage")));
+            this.btnBrowseFilePath.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnBrowseFilePath.Location = new System.Drawing.Point(418, 88);
+            this.btnBrowseFilePath.Name = "btnBrowseFilePath";
+            this.btnBrowseFilePath.Size = new System.Drawing.Size(24, 24);
+            this.btnBrowseFilePath.TabIndex = 107;
+            this.btnBrowseFilePath.UseVisualStyleBackColor = false;
+            this.btnBrowseFilePath.Click += new System.EventHandler(this.BtnBrowseFilePathClick);
+            // 
+            // ExportFileDlg
+            // 
+            this.AcceptButton = this.btnSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(454, 172);
+            this.Controls.Add(this.btnBrowseFilePath);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.filePathLabel);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.fileNameTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ExportFileDlg";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Export File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label filePathLabel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnBrowseFilePath;
+    }
+}
