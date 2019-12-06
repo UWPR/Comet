ccf22c195b712ab32bc17a7395912265d6912f2a
eng_jk <eng_jk@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2019-12-06 23:27:37 +0000

    update version string to 2019.01 rev. 3
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@1378 299d2d02-8f3b-485e-a07e-71e8e5e16577

--- branches/release_2019013/CometSearch/Common.h
+++ branches/release_2019013/CometSearch/Common.h
@@ -59,7 +59,7 @@ using namespace std;
 #include <iostream>
 #endif
 
-#define comet_version   "2019.01 rev. 2"
+#define comet_version   "2019.01 rev. 3"
 #define copyright "(c) University of Washington"
 
 // Redefined how the bin offset is interpreted and applied.  The valid range for the offset is
