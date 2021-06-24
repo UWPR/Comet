fe8225ae7a4911a6cbd3ff335d2f70b1add2c21e
eng_jk <eng_jk@299d2d02-8f3b-485e-a07e-71e8e5e16577>
2021-06-24 21:14:07 +0000

    tagging release 2021.01 rev. 0
    git-svn-id: https://svn.code.sf.net/p/comet-ms/code@1600 299d2d02-8f3b-485e-a07e-71e8e5e16577

---
+++ tags/release_2021010/CometSearch/CometMassSpecUtils.h
@@ -0,0 +1,58 @@
+/*
+   Copyright 2012 University of Washington
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+//  Definitions for generic mass spectrometry related utility functions.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _COMETMASSSPECUTILS_H_
+#define _COMETMASSSPECUTILS_H_
+
+const double Hydrogen_Mono = 1.007825035;
+const double Oxygen_Mono = 15.99491463;
+const double Carbon_Mono = 12.00000000;
+const double Nitrogen_Mono = 14.0030740;
+
+class CometMassSpecUtils
+{
+public:
+   static double GetFragmentIonMass(int iWhichIonSeries,
+                                    int i,
+                                    int ctCharge,
+                                    double *pdAAforward,
+                                    double *pdAAreverse);
+
+   static void AssignMass(double *pdAAMass,
+                          int bMonoMasses,
+                          double *dOH2);
+
+   static void GetProteinName(FILE *fpdb,
+                              comet_fileoffset_t lFilePosition,
+                              char *szProteinName);
+
+   static void GetProteinNameString(FILE *fpdb,
+                                    int iWhichQuery,  // which search
+                                    int iWhichResult, // which peptide within the search
+                                    int iPrintTargetDecoy,
+                                    vector<string>& vProteinTargets,  // the target protein names
+                                    vector<string>& vProteinDecoys);  // the decoy protein names if applicable
+
+   static string GetField(std::string *s,
+                          unsigned int n,
+                          char cDelimeter);
+};
+
+#endif // _COMETMASSSPECUTILS_H_
